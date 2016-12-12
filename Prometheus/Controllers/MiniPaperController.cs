3dacf71d0965bbd3ed97a1d57039143c6c2dd28c
Brad Qiu <brad.qiu@finisar.com>
2016-12-12 19:34:17 +0800

    Make Card go down without logic

--- Prometheus/Controllers/MiniPaperController.cs
+++ Prometheus/Controllers/MiniPaperController.cs
@@ -4,97 +4,335 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Domino.Models;
+using System.Reflection;
+using System.Web.Routing;
 
 namespace Domino.Controllers
 {
     public class MiniPaperController : Controller
     {
-        // GET: MiniPaper
+        // GET: MiniPapers
         public ActionResult ViewAll()
         {
+            var baseinfo = new ECOBaseInfo();
+            baseinfo.ECOKey = DominoVM.GetUniqKey();
+            baseinfo.ECONum = "97807";
+            baseinfo.PNDesc = "FCBG410QB1C10-FC";
+            baseinfo.Customer = "MRV";
+            baseinfo.PE = "Jessica Zheng";
+
             var vm = new List<List<DominoVM>>();
             var templist = new List<DominoVM>();
 
             var tempitem = new DominoVM();
-            tempitem.CardColor = DominoCardColor.green;
+            tempitem.ECOkey = baseinfo.ECOKey;
+            tempitem.Cardkey = DominoVM.GetUniqKey();
+            tempitem.CardStatus = DominoCardStatus.done;
             tempitem.CardContent = "ECO Pending";
             tempitem.CardNo = "1";
-            tempitem.CardModalName = "_ecosignoff1modal";
+            tempitem.CardModalName = "_ecopendingmodal";
+            tempitem.EBaseInfo = baseinfo;
             templist.Add(tempitem);
 
-            tempitem = new DominoVM();
-            tempitem.CardColor = DominoCardColor.blue;
-            tempitem.CardContent = "ECO signoff_1";
-            tempitem.CardNo = "2";
-            tempitem.CardModalName = "_ecosignoff1modal";
-            templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //tempitem.EBaseInfo = baseinfo;
+            //templist.Add(tempitem);
 
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //tempitem.EBaseInfo = baseinfo;
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //templist.Add(tempitem);
             vm.Add(templist);
 
+            //for (var idx = 0; idx < 10; idx++)
+            //{
+            //    templist = new List<DominoVM>();
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.done;
+            //    tempitem.CardContent = "ECO Pending";
+            //    tempitem.CardNo = "1";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    tempitem.EBaseInfo = baseinfo;
+            //    templist.Add(tempitem);
+
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    tempitem.EBaseInfo = baseinfo;
+            //    templist.Add(tempitem);
+
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    tempitem.EBaseInfo = baseinfo;
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    tempitem = new DominoVM();
+            //    tempitem.CardStatus = DominoCardStatus.working;
+            //    tempitem.CardContent = "ECO signoff_1";
+            //    tempitem.CardNo = "2";
+            //    tempitem.CardModalName = "_ecosignoff1modal";
+            //    templist.Add(tempitem);
+            //    vm.Add(templist);
+
+            //}
 
             return View(vm);
         }
 
-        public ActionResult CallCardModal(string ModalName)
+        public ActionResult CallCardModal(string ModalName,string ECOKey,string CardKey)
         {
+            var routedict = new RouteValueDictionary();
+            routedict.Add("ModalName", ModalName);
+            routedict.Add("ECOKey", ECOKey);
+            routedict.Add("CardKey", CardKey);
+
+            if (string.Compare(ModalName, "_ecopendingmodal") == 0)
+            {
+                return RedirectToAction("ECOPending", "MiniPaper", routedict);
+            }
+
+            return View("ViewAll");
+
+        }
+
+        public ActionResult ECOPending(string ModalName, string ECOKey, string CardKey)
+        {
+            var baseinfo = new ECOBaseInfo();
+            baseinfo.ECOKey = ECOKey;
+            baseinfo.ECONum = "97807";
+            baseinfo.PNDesc = "FCBG410QB1C10-FC";
+            baseinfo.Customer = "MRV";
+            baseinfo.PE = "Jessica Zheng";
+
             var vm = new List<List<DominoVM>>();
             var templist = new List<DominoVM>();
 
             var tempitem = new DominoVM();
-            tempitem.CardColor = DominoCardColor.green;
+            tempitem.ECOkey = ECOKey;
+            tempitem.Cardkey = CardKey;
+            tempitem.CardStatus = DominoCardStatus.done;
             tempitem.CardContent = "ECO Pending";
             tempitem.CardNo = "1";
-            tempitem.CardModalName = "_ecosignoff1modal";
+            tempitem.CardModalName = ModalName;
+            tempitem.EBaseInfo = baseinfo;
             templist.Add(tempitem);
-
-            tempitem = new DominoVM();
-            tempitem.CardColor = DominoCardColor.blue;
-            tempitem.CardContent = "ECO signoff_1";
-            tempitem.CardNo = "2";
-            tempitem.CardModalName = "_ecosignoff1modal";
-            templist.Add(tempitem);
-
             vm.Add(templist);
 
-            var taglist = new List<string>();
-            taglist.Add("A");
-            taglist.Add("B");
-            taglist.Add("C");
-            taglist.Add("D");
-            ViewBag.tobechoosetags = taglist;
             ViewBag.modalname = ModalName;
+            ViewBag.ECOKey = ECOKey;
+            ViewBag.CardKey = CardKey;
 
-            return View("ViewAll", vm);
+            return View("SingalECO", vm);
+
+
+            //var baseinfo = new ECOBaseInfo();
+            //baseinfo.ECONum = "97807";
+            //baseinfo.PNDesc = "FCBG410QB1C10-FC";
+            //baseinfo.Customer = "MRV";
+            //baseinfo.PE = "Jessica Zheng";
+
+
+            //var vm = new List<List<DominoVM>>();
+            //var templist = new List<DominoVM>();
+
+            //var tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.done;
+            //tempitem.CardContent = "ECO Pending";
+            //tempitem.CardNo = "1";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //tempitem.EBaseInfo = baseinfo;
+            //templist.Add(tempitem);
+
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.working;
+            //tempitem.CardContent = "ECO signoff_1";
+            //tempitem.CardNo = "2";
+            //tempitem.CardModalName = "_ecosignoff1modal";
+            //tempitem.EBaseInfo = baseinfo;
+            //templist.Add(tempitem);
+
+            //tempitem = new DominoVM();
+            //tempitem.CardStatus = DominoCardStatus.pending;
+            //tempitem.CardContent = "ECO complete";
+            //tempitem.CardNo = "3";
+            //tempitem.EBaseInfo = baseinfo;
+            //templist.Add(tempitem);
+
+            //vm.Add(templist);
+
+            //return View("ViewAll",vm);
         }
 
-        public ActionResult AddOne()
+        [HttpPost, ActionName("ECOPending")]
+        [ValidateAntiForgeryToken]
+        public ActionResult ECOPendingPost()
         {
+            var ECOKey = Request.Form["ECOKey"];
+            var CardKey = Request.Form["CardKey"];
+
+            var baseinfo = new ECOBaseInfo();
+            baseinfo.ECOKey = ECOKey;
+            baseinfo.ECONum = "97807";
+            baseinfo.PNDesc = "FCBG410QB1C10-FC";
+            baseinfo.Customer = "MRV";
+            baseinfo.PE = "Jessica Zheng";
+
             var vm = new List<List<DominoVM>>();
             var templist = new List<DominoVM>();
 
             var tempitem = new DominoVM();
-            tempitem.CardColor = DominoCardColor.green;
+            tempitem.ECOkey = ECOKey;
+            tempitem.Cardkey = CardKey;
+            tempitem.CardStatus = DominoCardStatus.done;
             tempitem.CardContent = "ECO Pending";
             tempitem.CardNo = "1";
-            tempitem.CardModalName = "_ecosignoff1modal";
+            tempitem.CardModalName = "_ecopendingmodal";
+            tempitem.EBaseInfo = baseinfo;
             templist.Add(tempitem);
 
             tempitem = new DominoVM();
-            tempitem.CardColor = DominoCardColor.blue;
-            tempitem.CardContent = "ECO signoff_1";
+            tempitem.ECOkey = ECOKey;
+            tempitem.Cardkey = DominoVM.GetUniqKey();
+            tempitem.CardStatus = DominoCardStatus.pending;
+            tempitem.CardContent = "ECO Sign off 1";
             tempitem.CardNo = "2";
             tempitem.CardModalName = "_ecosignoff1modal";
-            templist.Add(tempitem);
-
-            tempitem = new DominoVM();
-            tempitem.CardColor = DominoCardColor.red;
-            tempitem.CardContent = "ECO complete";
-            tempitem.CardNo = "3";
+            tempitem.EBaseInfo = baseinfo;
             templist.Add(tempitem);
 
             vm.Add(templist);
 
-            return View("ViewAll",vm);
+            //ViewBag.modalname = "_ecopendingmodal";
+            //ViewBag.ECOKey = ECOKey;
+            //ViewBag.CardKey = CardKey;
+
+            return View("SingalECO", vm);
         }
+
     }
 }
--- Prometheus/Models/DominoVM.cs
+++ Prometheus/Models/DominoVM.cs
@@ -1,22 +1,101 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 
 namespace Domino.Models
 {
-    public class DominoCardColor
+    public class DominoCardStatus
     {
-        public static string red = "red";
-        public static string blue = "blue";
-        public static string green = "green";
+        public static string pending = "pending";
+        public static string working = "working";
+        public static string done = "done";
+        public static string info = "info";
+    }
+
+    public class ECOBaseInfo
+    {
+        public string ECOKey { set; get; }
+        public string ECONum { set; get; }
+        public string PNDesc { set; get; }
+        public string Customer { set; get; }
+        public string Complex { set; get; }
+        public string RSM { set; get; }
+        public string PE { set; get; }
+        public string RiskBuild { set; get; }
+        public string InitRevison { set; get; }
+        public string FinalRevison { set; get; }
+        public string TLAAvailable { set; get; }
+        public string OpsEntry { set; get; }
+        public string TestModification { set; get; }
+        public string ECOSubmit { set; get; }
+        public string ECOReviewSignoff { set; get; }
+        public string ECOCCBSignoff { set; get; }
+        public string QTRInit { set; get; }
+
+        private Dictionary<string, string> namedict = new Dictionary<string, string>();
+        public Dictionary<string, string> NameDict { get { return namedict; } }
+        public ECOBaseInfo()
+        {
+            namedict.Clear();
+            namedict.Add("ECONum", "ECO Number");
+            namedict.Add("PNDesc", "Product Requested");
+            namedict.Add("Customer", "Customer");
+            namedict.Add("Complex", "Complex");
+            namedict.Add("RSM", "RSM");
+            namedict.Add("PE", "PE");
+            namedict.Add("RiskBuild", "Risk Build");
+            namedict.Add("InitRevison", "Initial Mini-PIP Received");
+            namedict.Add("FinalRevison", "Final Mini-PIP Revision");
+            namedict.Add("TLAAvailable", "TLA Available");
+            namedict.Add("OpsEntry", "Ops Log Entry");
+            namedict.Add("TestModification", "Test Modification");
+            namedict.Add("ECOSubmit", "ECO Submit");
+            namedict.Add("ECOReviewSignoff", "ECO TECH Review");
+            namedict.Add("ECOCCBSignoff", "ECO CCB Signoff");
+            namedict.Add("QTRInit", "QTR Initiation");
+        }
+
+        public static List<KeyValuePair<string, string>> RetrieveBaseInfo(ECOBaseInfo info)
+        {
+            var ret = new List<KeyValuePair<string, string>>();
+            PropertyInfo[] properties = typeof(ECOBaseInfo).GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                if (info.NameDict.ContainsKey(property.Name))
+                {
+                    var val = string.Empty;
+                    try { val = Convert.ToString(property.GetValue(info)); }
+                    catch (Exception ex) { val = string.Empty; }
+
+                    if (!string.IsNullOrEmpty(val))
+                    {
+                        ret.Add(new KeyValuePair<string,string>(info.NameDict[property.Name], val));
+                    }
+                }//end if
+
+            }//end foreach
+
+            return ret;
+        }
+
+
     }
 
     public class DominoVM
     {
+        public ECOBaseInfo EBaseInfo { set; get; }
+        public string ECOkey { set; get; }
+        public string Cardkey { set; get; }
         public string CardNo { set; get; }
         public string CardContent { set; get; }
-        public string CardColor { set; get; }
+        public string CardStatus { set; get; }
         public string CardModalName { set; get; }
+        
+        public static string GetUniqKey()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
     }
 }
